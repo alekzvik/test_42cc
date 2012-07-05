eb13cb6f788a197aee883969bd71c03bcf349327
Alex Vykalyuk <alekzvik@gmail.com>
2012-07-05 19:32:18 +0300

    ticket:3 Initial commit with tests and a model

---
+++ requests_log/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class RequestEntry(models.Model):
+    path = models.TextField()
+    method = models.CharField(max_length=200)
+    query = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
