b1ce91b2cc5fddb9ba675f4302a9c1a3c75f9684
Alex Vykalyuk <alekzvik@gmail.com>
2012-07-05 21:08:27 +0300

    ticket:3 Added url for requests page to contact template

--- requests_log/urls.py
+++ requests_log/urls.py
@@ -3,7 +3,8 @@ from django.views.generic import ListView
 from requests_log.models import RequestEntry
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(queryset=RequestEntry.objects.all()[:10],
-                                context_object_name='requests',
-                                template_name="requests.html")),
+    url(r'^$', ListView.as_view(
+        queryset=RequestEntry.objects.all()[:10],
+        context_object_name='requests',
+        template_name="requests.html"), name='requests_view'),
 )
