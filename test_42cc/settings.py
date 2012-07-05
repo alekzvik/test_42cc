80d0118e5a69a0e89b0a2a8cdb840e1c0218c0f7
Alex Vykalyuk <alekzvik@gmail.com>
2012-07-05 19:47:18 +0300

    ticket:3 Tests fail

--- test_42cc/settings.py
+++ test_42cc/settings.py
@@ -124,6 +124,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'contact',
+    'requests_log',
 )
 
 # A sample logging configuration. The only tangible logging
