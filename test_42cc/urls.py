9692d20e2fbca141542a64d004ba62347a60fe79
Alex Vykalyuk <alekzvik@gmail.com>
2012-07-05 13:34:47 +0300

    ticket:1 Isolated urls for contact app

--- test_42cc/urls.py
+++ test_42cc/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'test_42cc.views.home', name='home'),
+    url(r'^', include('contact.urls')),
     # url(r'^test_42cc/', include('test_42cc.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
