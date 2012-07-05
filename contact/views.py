0a6f4947b6028040a7155b222eeb86cabe997af3
Alex Vykalyuk <alekzvik@gmail.com>
2012-07-05 13:33:42 +0300

    ticket:1 Added templates

--- contact/views.py
+++ contact/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+from contact.models import Contact
+
+
+def index(request):
+    info = get_object_or_404(Contact)
+    return render_to_response('index.html', {'info': info},
+        context_instance=RequestContext(request))
