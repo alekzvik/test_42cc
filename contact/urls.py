3cabe8262b6aff015a5070e5218a5310dca50a5b
Alex Vykalyuk <alekzvik@gmail.com>
2012-07-09 18:22:28 +0300

    ticket:5 Created view for contact_edit form

--- contact/urls.py
+++ contact/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, url
+from contact.views import ContactUpdate
+from django.contrib.auth.decorators import login_required
 
-urlpatterns = patterns('contact.views',
-    url(r'^$', 'index'),
+
+urlpatterns = patterns('',
+    url(r'^$', 'contact.views.index'),
+    url(r'^edit/$', login_required(ContactUpdate.as_view()),
+        name='contact_edit'),
 )
