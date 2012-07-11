8d100d79ac0f94a9105b591ede884a7b5e99c99a
Alex Vykalyuk <alekzvik@gmail.com>
2012-07-11 21:18:17 +0300

    ticket:10 Fixed mistakes in tests

--- models_log/tests.py
+++ models_log/tests.py
@@ -8,32 +8,32 @@ class TestModel(models.Model):
 
 
 class ModelLogTest(TestCase):
+    def setUp(self):
+        TestModel.objects.create(number=0)
+
     def test_create(self):
-        count = ModelLog.objects.count()
-        TestModel.create(number=0)
-        self.assertEqual(ModelLog.objects.count(), count + 1)
         item = ModelLog.objects.latest()
-        self.assertEqual(item.model_name, 'TestModel')
+        self.assertEqual(item.model_name, 'models_log.testmodel')
         self.assertEqual(item.action, 'create')
         self.assertEqual(item.changed_pk, 1)
 
     def test_update(self):
         count = ModelLog.objects.count()
-        data = TestModel.object.get(pk=1)
+        data = TestModel.objects.get(pk=1)
         data.number = 1
         data.save()
         self.assertEqual(ModelLog.objects.count(), count + 1)
         item = ModelLog.objects.latest()
-        self.assertEqual(item.model_name, 'TestModel')
+        self.assertEqual(item.model_name, 'models_log.testmodel')
         self.assertEqual(item.action, 'update')
         self.assertEqual(item.changed_pk, 1)
 
     def test_delete(self):
         count = ModelLog.objects.count()
-        data = TestModel.object.get(pk=1)
+        data = TestModel.objects.get(pk=1)
         data.delete()
         self.assertEqual(ModelLog.objects.count(), count + 1)
         item = ModelLog.objects.latest()
-        self.assertEqual(item.model_name, 'TestModel')
+        self.assertEqual(item.model_name, 'models_log.testmodel')
         self.assertEqual(item.action, 'delete')
         self.assertEqual(item.changed_pk, 1)
