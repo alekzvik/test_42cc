e7f90ed1564bb011b95673cf080dc89ce433eff4
Alex Vykalyuk <alekzvik@gmail.com>
2012-07-11 12:39:52 +0300

    ticket:6 Added ajax to form

--- contact/forms.py
+++ contact/forms.py
@@ -1,5 +1,17 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, DateInput
 from contact.models import Contact
+
+
+class CalendarWidget(DateInput):
+    class Media:
+        css = {
+            'all': ('css/jquery-ui-1.8.21.custom.css', )
+        }
+        js = (
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
+            'js/jquery-ui-1.8.21.custom.min.js',
+            'js/calendar.js',
+        )
 
 
 class ContactForm(ModelForm):
@@ -7,3 +19,12 @@ class ContactForm(ModelForm):
         model = Contact
         fields = ('name', 'last_name', 'birth_date', 'photo', 'email',
             'jabber', 'skype', 'other_contacts', 'bio')
+        widgets = {
+            'birth_date': CalendarWidget(),
+        }
+
+    class Media:
+        js = (
+            'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
+            'js/contact_form.js'
+        )
---
+++ contact/static/js/contact_form.js
@@ -0,0 +1,15 @@
+jQuery(function() {
+    var form = jQuery("#ContactForm");
+    form.submit(function(e) {
+        jQuery("#id_sendbutton").attr('disabled', true)
+        jQuery("#sendwrapper").prepend('<span>Sending message, please wait... </span>')
+        jQuery("#ajaxwrapper").load(
+            form.attr('action') + ' #ajaxwrapper',
+            form.serializeArray(),
+            function(responseText, responseStatus) {
+                jQuery("#id_sendbutton").attr('disabled', false)
+            }
+        );
+        e.preventDefault();
+    });
+});
