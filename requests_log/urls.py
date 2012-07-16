4056c1385a915e58942bf653b97689fcdcf22362
Alex Vykalyuk <alekzvik@gmail.com>
2012-07-16 17:34:45 +0300

    ticket:13 Added ability to change request priority

--- requests_log/urls.py
+++ requests_log/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, url
-from django.views.generic import ListView
-from requests_log.models import RequestEntry
 
 urlpatterns = patterns('',
-    url(r'^$', ListView.as_view(
-        queryset=RequestEntry.objects.all()[:10],
-        context_object_name='requests',
-        template_name="requests.html"), name='requests_view'),
+    url(r'^$', 'requests_log.views.requests_view', name='requests_view'),
+    url(r'^priority_update/$', 'requests_log.views.priority_update'),
 )
--- requests_log/views.py
+++ requests_log/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from django.shortcuts import redirect, render
+from requests_log.forms import RequestEntryFormSet
+from django.contrib.auth.decorators import login_required
+from requests_log.models import RequestEntry
+
+
+def requests_view(request):
+    requests = RequestEntry.objects.all()[:10]
+    formset = RequestEntryFormSet(queryset=requests)
+    objects = zip(requests, formset)
+    return render(request, "requests.html", {'objects': objects, 'formset': formset})
+
+
+@login_required
+def priority_update(request):
+    if request.method == 'POST':
+        formset = RequestEntryFormSet(request.POST)
+
+        if formset.is_valid():
+            formset.save()
+
+    return redirect('requests_view')
